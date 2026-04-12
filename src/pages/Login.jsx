32c8c075eb03694ba35b7439c57455ca633dc06e
codArtic0 <raulsousadesigner@gmail.com>
2026-04-12 15:55:30 -0300

    STYLE: Implementação de logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,13 +6,18 @@ export default function Login(){
     <div className="container-login">
         <div className="card-login">
             <form className="login">
+                <div className="container-logo">
+                    <div className="logo">
+                        <img src="/FitForge.png" alt="Logo Fit Forge" />
+                    </div>
+                </div>
                 <h1 className="title">Acesse o sistema</h1>
                 <div className="input-box">
                     <input type="number" placeholder="ID" className="input-label"></input>
                     <FaUser className="icon"></FaUser>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Nome" className="input-label"></input>
+                    <input type="password" placeholder="Senha" className="input-label"></input>
                     <FaLock className="icon"></FaLock>
                 </div>
                 <button>Entrar</button>
