899f7f76e3a853210fb86ca53c5de839c6e8d119
codArtic0 <raulsousadesigner@gmail.com>
2026-04-14 15:08:00 -0300

    FIX: Página de Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
+import "../styles/notfound.css"
+
 export default function NotFound() {
     return(
-        <h1>404, página não encontrada</h1>
+        <div className="nf-container">
+            <div className="urso">
+                <img src="/confusedBear.png" alt="Urso confuso"/>
+            </div>
+            <div className="text-container">
+                <h1>404, página não encontrada</h1>
+                <h2>Lamentamos, mas essa página não existe!</h2>
+            </div>
+        </div>
     )
 }
