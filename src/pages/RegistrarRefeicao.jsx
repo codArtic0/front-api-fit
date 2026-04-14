910b0e380775c3182cee7094505481335eb6f83c
codArtic0 <raulsousadesigner@gmail.com>
2026-04-14 16:34:41 -0300

    ADD: Página de Adicionar Refeição (prototipo)

--- src/pages/RegistrarRefeicao.jsx
+++ src/pages/RegistrarRefeicao.jsx
@@ -1,7 +1,21 @@
+import "../styles/refeicao.css"
+
 export default function RegistrarRefeicao(){
     return(
         <>
-            <h1>Registrar Refeicao</h1>
+            <div className="refeicao-container">
+                <div className="card-refeicao">
+                    <h1>REGISTRAR REFEIÇÃO</h1>
+                    <form className="form-refeicao">
+                        <div className="input-group">
+                            <input type="text" placeholder="Nome do alimento"/>
+                            <input type="number" placeholder="Quantidade (g)"/>
+                        </div>
+                        <button className="confirmar-button"> Confirmar</button>
+                    </form>
+                </div>
+             </div>
+
         </>
     )
 }
