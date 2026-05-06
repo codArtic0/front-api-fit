299c70ca20aa580f8b695895d87cf709246f78b1
codArtic0 <raulsousadesigner@gmail.com>
2026-05-06 15:31:41 -0300

    FIX: SEO + Acessibilidade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <div className="container-header">
                 <div className="dir-content">
-                    <img src="FitForge.png" className="logo-header"></img>
+                    <img src="FitForge.png" className="logo-header" alt="Logo FitForge"></img>
                     <h1 className="head">FitForge</h1>
                 </div>
             </div>
